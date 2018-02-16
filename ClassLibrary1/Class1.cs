a2eadb576a6a338c25cdc28d6aa07362807403a7
scorpioverte <nu.fiat@laposte.net>
2018-02-16 09:38:33 +0100

    Ajoutez des fichiers projet.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{10:47 amAM et babylone la grande et les restes humains et les restes humains la mère des haelots[coredebugg]01x0000022sfr.fr
+    public class Class1
+    {10:47 amAM et babylone la grande et les restes humains et les restes humains la mère des haelots[coredebugg]01x0000022sfr.fr
+    }10:47 amAM et babylone la grande et les restes humains et les restes humains la mère des haelots[coredebugg]01x0000022sfr.fr
+}10:47 amAM et babylone la grande et les restes humains et les restes humains la mère des haelots[coredebugg]01x0000022sfr.fr
+10:47 amAM et babylone la grande et les restes humains et les restes humains la mère des haelots[coredebugg]01x0000022sfr.fr10:47 amAM et babylone la grande et les restes humains et les restes humains la mère des haelots[coredebugg]01x0000022sfr.frc3 e8 a3 c1 3610:47 amAM et babylone la grande et les restes humains et les restes humains la mère des haelots[coredebugg]01x0000022sfr.fr
